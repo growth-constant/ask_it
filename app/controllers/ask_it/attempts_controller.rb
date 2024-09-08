b8d163f2d34525462e52a097d8c87832af5d111f
Victor Fernandez <victor.j.fdez@gmail.com>
2024-09-08 12:26:54 -0600

    removed all test specs and created controllers in ask_it

---
+++ app/controllers/ask_it/attempts_controller.rb
@@ -0,0 +1,32 @@
+module AskIt
+  # Handles creation and management of survey attempts
+  class AttemptsController < ApplicationController
+    helper 'ask_it/surveys'
+
+    def new
+      @survey = Survey.active.last
+      @attempt = @survey.attempts.new
+      @attempt.answers.build
+      @participant = current_user # Adjust as needed
+    end
+
+    def create
+      @survey = Survey.active.last
+      @attempt = @survey.attempts.new(attempt_params)
+      @attempt.participant = current_user # Adjust as needed
+
+      if @attempt.save
+        redirect_to new_ask_it_attempt_path, notice: t('.success')
+      else
+        flash.now[:error] = @attempt.errors.full_messages.join(', ')
+        render :new
+      end
+    end
+
+    private
+
+    def attempt_params
+      params.require(:attempt).permit(answers_attributes: %i[question_id option_id option_text option_number predefined_value_id])
+    end
+  end
+end
---
+++ app/controllers/ask_it/surveys_controller.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+module AskIt
+  # Controller for managing surveys in the AskIt module
+  class SurveysController < ApplicationController
+    before_action :load_survey, only: %i[show edit update]
+
+    def index
+      @surveys = AskIt::Survey.all
+    end
+
+    def new
+      @survey = AskIt::Survey.new
+    end
+
+    def create
+      @survey = AskIt::Survey.new(survey_params)
+      if @survey.valid? && @survey.save
+        redirect_to surveys_path, notice: I18n.t('surveys_controller.create')
+      else
+        render :new
+      end
+    end
+
+    def edit
+      # @survey is already loaded by before_action
+    end
+
+    def show
+      # @survey is already loaded by before_action
+    end
+
+    def update
+      if @survey.update(survey_params)
+        redirect_to surveys_path, notice: I18n.t('surveys_controller.update')
+      else
+        render :edit
+      end
+    end
+
+    private
+
+    def load_survey
+      @survey = AskIt::Survey.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      redirect_to surveys_path, alert: 'Survey not found'
+    end
+
+    def survey_params
+      params.require(:survey).permit(
+        :name, :description, :active,
+        sections_attributes: [:id, :name, :_destroy,
+                              { questions_attributes: [:id, :text, :question_type, :_destroy,
+                                                       { options_attributes: %i[id text _destroy] }] }]
+      )
+    end
+  end
+end
